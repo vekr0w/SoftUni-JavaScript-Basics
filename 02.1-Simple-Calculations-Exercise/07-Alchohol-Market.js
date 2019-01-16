07e0f3fb1564671c044e0cbba0adb15fd26913c1
vekr0w <vekr0w@gmail.com>
2019-01-16 11:10:19 +0200

    Alc Market

--- 02.1-Simple-Calculations-Exercise/07-Alchohol-Market.js
+++ 02.1-Simple-Calculations-Exercise/07-Alchohol-Market.js
@@ -1,14 +1,17 @@
 function alchoholMarket(input) {
 
     let whiskeyPrice = Number(input[0]);
+
     let beerCountLiters = Number(input[1]);
     let wineCountLiters = Number(input[2]);
     let rakiaCountLiters = Number(input[3]);
     let whiskeyCountLiters = Number(input[4]);
 
+
     let rakiaPrice = whiskeyPrice / 2;
     let beerPrice = rakiaPrice - (rakiaPrice * 0.80);
     let winePrice = rakiaPrice - (rakiaPrice * 0.40);
+
     let sumRakia = rakiaCountLiters * rakiaPrice;
     let sumWine = wineCountLiters * winePrice;
     let sumBeer = beerCountLiters * beerPrice;
@@ -16,8 +19,6 @@ function alchoholMarket(input) {
     let sumTotal = sumBeer + sumRakia + sumWhiskey + sumWine;
 
     console.log(sumTotal.toFixed(2));
-
-
 
 }
 alchoholMarket([50, 10, 3.5, 6.5, 1]);
