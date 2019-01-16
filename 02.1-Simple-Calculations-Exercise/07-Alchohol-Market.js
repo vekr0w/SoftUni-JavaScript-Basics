bcde82149e4ecaf2140194f993d5fe3a68b695b8
vekr0w <vekr0w@gmail.com>
2019-01-16 11:09:48 +0200

    Last exercise from this homework.

--- 02.1-Simple-Calculations-Exercise/07-Alchohol-Market.js
+++ 02.1-Simple-Calculations-Exercise/07-Alchohol-Market.js
@@ -7,11 +7,17 @@ function alchoholMarket(input) {
     let whiskeyCountLiters = Number(input[4]);
 
     let rakiaPrice = whiskeyPrice / 2;
-    let beerPrice = rakiaPrice * 0.80;
-    let winePrice = rakiaPrice * 0.40;
+    let beerPrice = rakiaPrice - (rakiaPrice * 0.80);
+    let winePrice = rakiaPrice - (rakiaPrice * 0.40);
+    let sumRakia = rakiaCountLiters * rakiaPrice;
+    let sumWine = wineCountLiters * winePrice;
+    let sumBeer = beerCountLiters * beerPrice;
+    let sumWhiskey = whiskeyCountLiters * whiskeyPrice;
+    let sumTotal = sumBeer + sumRakia + sumWhiskey + sumWine;
 
-    console.log(winePrice);
+    console.log(sumTotal.toFixed(2));
 
 
 
 }
+alchoholMarket([50, 10, 3.5, 6.5, 1]);
