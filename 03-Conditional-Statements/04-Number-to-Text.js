34c49e566ad304dc48a128ec02545b681fba2baa
vekr0w <vekr0w@gmail.com>
2019-01-19 16:47:13 +0200

    Template

--- 03-Conditional-Statements/04-Number-to-Text.js
+++ 03-Conditional-Statements/04-Number-to-Text.js
@@ -1,32 +1,33 @@
 function numberToText(input) {
     let number = Number(input[0]);
+
     switch (number) {
         case 1:
             console.log(`one`);
             break;
         case 2:
-            console.log(`two`);;
+            console.log(`two`);
             break;
         case 3:
-            console.log(`three`);;
+            console.log(`three`);
             break;
         case 4:
-            console.log(`four`);;
+            console.log(`four`);
             break;
         case 5:
-            console.log(`five`);;
+            console.log(`five`);
             break;
         case 6:
-            console.log(`six`);;
+            console.log(`six`);
             break;
         case 7:
-            console.log(`seven`);;
+            console.log(`seven`);
             break;
         case 8:
-            console.log(`eight`);;
+            console.log(`eight`);
             break;
         case 9:
-            console.log(`nine`);;
+            console.log(`nine`);
             break;
         default:
             console.log(`number too big`);
